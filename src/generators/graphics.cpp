b2d79957026574c4cea4d671e991d0be9ba52f66
phiwen96 <philip.sve@hotmail.com>
2021-02-27 14:32:06 +0100

    a

---
+++ src/generators/graphics.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+      system("echo \"struct snoppen{};\" > graphics_info.hpp");
+      std::cout << argc << " " << argv[0] << std::endl;
+      return 0;
+}
