da65eee534d15956ad91af520e371ec49b3b8aed
phiwen96 <philip.sve@hotmail.com>
2021-03-01 12:08:32 +0100

    a

--- xbuild/_deps/catch-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/pw/GitHub/phulkan/xbuild/_deps/catch-src
-# Build directory: /Users/pw/GitHub/phulkan/xbuild/_deps/catch-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- xbuild/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/pw/GitHub/phulkan/docs
-# Build directory: /Users/pw/GitHub/phulkan/xbuild/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
