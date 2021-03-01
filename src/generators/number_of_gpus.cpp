ae5d301d909c659a683e1c40c0e59d78c7341780
phiwen96 <philip.sve@hotmail.com>
2021-03-01 13:15:14 +0100

    a

--- src/generators/number_of_gpus.cpp
+++ src/generators/number_of_gpus.cpp
@@ -4,8 +4,14 @@
 
 int main (int argc, const char * argv[])
 {
+//      file <write> f2 (argv [0]);
+//      f2 << "snopp";
+      
       file <write> f ("gpus_info.hpp");
-      f << "#pragma once\n#define number_of_gpus 0";
+//      file <write> f (argv [0]);
+      f << "#pragma once\n#define number_of_gpus 0\n";
+      f << "#define number_of_gpus2 0";
+      
       
 	return 0;
 }
